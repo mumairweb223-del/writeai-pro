a1615407655e2c21684b02bfff50a9075d038e28
mumairweb223-del <mumairweb223@gmail.com>
2026-06-14 02:34:49 +0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,3 +1,4 @@
+console.log("API Key loded:", process.env.OPENROUTER_API_KEY ? "Yes" : "No");
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
